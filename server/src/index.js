90b3d68a68c8619f979c7714bda17161f7d73630
Franco <francocorniglione5@gmail.com>
2023-11-29 19:48:36 -0300

    setting and configurating apollo server

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
+
+const port = process.env.PORT || 4000;
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache })
+        }
+      };
+    },
+    listen: { port }
+  });
+
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
