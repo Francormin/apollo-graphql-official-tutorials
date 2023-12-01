6d8010c68fb6d8d32757c1d1f17d994b70b350a4
Franco <francocorniglione5@gmail.com>
2023-12-01 09:31:37 -0300

    homepage with all cat cards

---
+++ client/src/pages/tracks.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import { Layout, QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
+
+/** TRACKS query to retrieve all tracks */
+const GET_TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  const { loading, error, data } = useQuery(GET_TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map(track => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Tracks;
